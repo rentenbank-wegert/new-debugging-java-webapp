292acd254e543635ecd6c53d756ca91b25a491fe
rentenbank-wegert <bernd.wegert@rentenbank.de>
2025-03-10 17:35:39 +0000

    Serverconfig für Codespaceerstellung gespeichert

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,6 +4,7 @@ echo "--------------------------"
 
 cd .devcontainer
 
+# Server Download und Bereitstellung
 wget https://archive.apache.org/dist/tomcat/tomcat-11/v11.0.0-M6/bin/apache-tomcat-11.0.0-M6.tar.gz
 tar -xzf apache-tomcat-11.0.0-M6.tar.gz
 #mkdir -p /home/vscode/.rspWegert001/redhat-community-server-connector/runtimes/installations/tomcat-11.0.0-M6/
@@ -11,4 +12,6 @@ tar -xzf apache-tomcat-11.0.0-M6.tar.gz
 mkdir -p /home/vscode/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-11.0.0-M6/
 mv apache-tomcat-11.0.0-M6 /home/vscode/.rsp/redhat-community-server-connector/runtimes/installations/tomcat-11.0.0-M6/
 
-cp /workspaces/new-debugging-java-webapp/.devcontainer/tmpServerConnector-apache-tomcat-11.0.0-M6--263-QmZp4o8oN0m8-.json /tmp/
+# Serverconfig kopieren
+mkdir -p /home/vscode/.rsp/redhat-community-server-connector/servers/
+cp /workspaces/new-debugging-java-webapp/.devcontainer/tmpServerConnector-apache-tomcat-11.0.0-M6--263-QmZp4o8oN0m8-.json /home/vscode/.rsp/redhat-community-server-connector/servers/apache-tomcat-11.0.0-M6
